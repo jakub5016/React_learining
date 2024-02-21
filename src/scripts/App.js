b69ecf48e6d2b16ecba66413d54dc5d254ff3f27
jakub5016 <me.jakub.gil@gmail.com>
2024-02-21 16:14:59 +0100

    Updated directory structure

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import '../css/App.css';
 import Navbar from './Navbar';
 import Content from './Content'
 // Bootstrap CSS
--- src/scripts/Content.js
+++ src/scripts/Content.js
@@ -1,5 +1,5 @@
 import { useState, useEffect  } from 'react';
-import './Content.css';
+import '../css/Content.css';
 
 
 
